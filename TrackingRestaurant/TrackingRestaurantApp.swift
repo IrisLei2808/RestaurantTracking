766b6de9eeb9fcb6a03ef4e85519a2fda2b0a930
IrisLei2808 <duclt2808@gmail.com>
2023-06-02 23:35:18 -0700

    Initial Commit

---
+++ TrackingRestaurant/TrackingRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackingRestaurantApp.swift
+//  TrackingRestaurant
+//
+//  Created by Duc Le on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackingRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
